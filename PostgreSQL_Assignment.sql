7c2fc5d6a43dc164766ef570fdd724d88b1482eb
Sohelrana2815 <sohel152302@gmail.com>
2025-05-27 12:18:50 +0600

    successfully solved all 9 queries

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,7 +1,4 @@
--- Active: 1748269062425@@127.0.0.1@5432@wildlife_monitoring_db@public
-
-
-
+-- TABLE CREATION --
 CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -29,11 +26,18 @@ CREATE TABLE sightings (
 );
 
 
--- Problem 1
-INSERT INTO rangers (name,region)  VALUES
-('Derek Fox', 'Coastal Plains');
+-- SAMPLE DATA INSERTION
+
+INSERT INTO rangers (name, region) VALUES
+('Alice Green', ' Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range');
+
+
+
 
 INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia','1775-01-01', 'Endangered'),
 ('Bengal Tiger', 'Panthera tigris','1758-01-01', 'Endangered'),
 ('Red Panda','Ailurus fulgens','1825-01-01','Vulnerable'),
 ('Asiatic Elephant','Elephas maximus indicus','1758-01-01','Endangered');
@@ -41,20 +45,24 @@ INSERT INTO species (common_name, scientific_name, discovery_date, conservation_
 
 
 
-
-
-
-
-
 INSERT INTO sightings (species_id,ranger_id, location, sighting_time, notes) VALUES
+(1,1,'Peak Ridge','2024-05-10 07:45:00','Camera trap image captured'),
+(2,2,'Bankwood Area','2024-05-12 16:20:00','Juvenile seen'),
+(3,3,'Bamboo Grove East','2024-05-15 09:10:00','Feeding observed'),
+(1,2,'Snowfall Pass','2024-05-18 18:30:00',NULL);
 
-(1,2,'Gulistan Pass','2024-06-19 19:30:00',NULL);
 
 
+
+
+
+
+-- Problem 1 --
+INSERT INTO rangers (name,region)  VALUES
+('Derek Fox','Coastal Plains');
 
 -- Problem 2
 SELECT count(DISTINCT species_id) AS unique_species_count FROM sightings
-
 
 -- Problem 3 
 
@@ -65,21 +73,14 @@ SELECT * FROM sightings WHERE location LIKE '%Pass%'
 -- Problem 4
 
 SELECT rangers.name AS ranger_name, COUNT(sightings.sighting_id) AS total_sightings FROM rangers
-JOIN sightings ON rangers.ranger_id = sightings.ranger_id -- OR you can use USING (ranger_id) because we have common column/attribute "ranger_id" in both rangers and sightings table
+JOIN sightings ON rangers.ranger_id = sightings.ranger_id -- OR we can use USING for common column for both table
 GROUP BY rangers.name
 
--- Show all sightings count even if a ranger don't have any sighing count
+-- Show all sightings count even if a ranger don't have any sighing count (Optional)
 
 SELECT rangers.name AS ranger_name, COUNT(sightings.sighting_id) AS total_sightings FROM rangers
 LEFT JOIN sightings  ON rangers.ranger_id = sightings.ranger_id
 GROUP BY rangers.name
-
-
-
-
-Select * FROM species
-
-SELECT * FROM sightings
 
 
 
@@ -90,17 +91,6 @@ FROM species
 LEFT JOIN sightings ON species.species_id = sightings.species_id
 WHERE sightings.sighting_id IS NULL
 
-
-
--- Problem 6
-
-
-
-Select * FROM species
-
-SELECT * FROM rangers
-
-SELECT * FROM sightings;
 
 
 -- Problem 6 
@@ -119,12 +109,6 @@ ORDER BY sightings.sighting_time DESC
 LIMIT 2
 
 
-
-
-SELECT * FROM species;
-
-
-
 -- Problem 7
 
 UPDATE species
@@ -136,8 +120,21 @@ FROM discovery_date) < 1800
 
 
 
+-- Problem 8
+
+SELECT sighting_id,
+CASE
+WHEN EXTRACT(HOUR FROM sighting_time) >=0 AND EXTRACT(HOUR FROM sighting_time) <12 THEN 'Morning'
+WHEN EXTRACT(HOUR FROM sighting_time)  >=12 AND EXTRACT (HOUR FROM sighting_time) <17 THEN 'Afternoon'
+ELSE 'Evening'
+END AS time_of_day
+FROM sightings;
 
 
 
 
 
+
+-- Problem 9 
+DELETE FROM rangers
+WHERE ranger_id NOT IN (SELECT ranger_id FROM sightings)
